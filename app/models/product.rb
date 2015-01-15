cb8051b84e0143152c4b3cc1ab8ed70f142e1500
IuliaMada <iulia.valeria.md@gmail.com>
2015-01-15 14:21:42 +0200

    Add CSS

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-	validates :title, uniqueness: true
+	validates :title, uniqueness: true, length: {minimum: 10, message: 'teapa!!'}
 	validates :image_url, allow_blank: true, format: {
 				with:
 				%r{\.(gif|jpg|png|jpeg)\Z}i,
 				message: 'must be a URL for GIF, JPG, JPEG or PNG image.'
 				}
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -44,6 +44,7 @@ class ProductTest < ActiveSupport::TestCase
 			assert new_product(name).invalid?, "#{name} shouldn't be valid"
 		end
 	end
+
 	test "product is not valid without a unique title" do
 		product = Product.new(title: products(:ruby).title,
 													description: "yyy",
@@ -52,4 +53,14 @@ class ProductTest < ActiveSupport::TestCase
 		assert product.invalid?
 		assert_equal ["has already been taken"], product.errors[:title]
 	end
+
+	test "title length shoudn't be less than 10" do
+		product = Product.new(title: "Scurt",
+													description: "yyy",
+													price: 1,
+													image_url: "fred.gif")
+			assert product.invalid?
+			assert_equal ["teapa!!"], product.errors[:title]
+	end
+
 end
